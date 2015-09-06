7995906a437fe4357b92dd103388d0e4764c926b
François Févotte <fevotte@gmail.com>
2015-09-06 23:56:08 +0200

    Simplify implementation
    This is based on ideas by Jordon Biondo on Emacs Stack Exchange:
       http://emacs.stackexchange.com/a/16337/221
    
    The implementation is simplified a lot: scratch buffers are now
    implemented as non-file buffers. We simply need to make sure we are
    prompted to save them before killing them or quitting emacs.

--- scratch.el
+++ scratch.el
@@ -1,6 +1,7 @@
-;;; scratch.el --- Manage scratch buffers
+;;; scratch.el --- Avoid data loss for scratch buffers
 
-;; Package-Requires: ((names "0.5") (emacs "24"))
+;; Package-Requires: ((emacs "24"))
+;; Version: 0.2
 
 ;;; Commentary:
 ;;
@@ -13,18 +14,12 @@
   "Manage scratch buffers"
   :group 'editing)
 
-(defcustom scratch/directory "/scratch-buffer/"
-  "Prefix path for scratch buffers.
-It is safer if this directory doesn't exist."
-  :type  'string
-  :group 'scratch)
-
 (defcustom scratch/default-name "*scratch*"
   "Default name for scratch buffers"
   :type  'string
   :group 'scratch)
 
-(defcustom scratch/create-when-blank-name nil
+(defcustom scratch/create-when-blank-name t
   "If non-nil, leaving the buffer name blank in
   `scratch/switch-to-buffer' creates a new scratch buffer.
   Otherwise, switch to the default scratch buffer."
@@ -35,12 +30,11 @@ It is safer if this directory doesn't exist."
 ;; * Entry points
 
 (defun scratch/create (buffer-name)
-  (interactive (list
-                (generate-new-buffer-name
-                 (if current-prefix-arg
-                     (read-from-minibuffer "Create scratch buffer: ")
-                   scratch/default-name))))
-  (switch-to-buffer (scratch--get-buffer-create buffer-name)))
+  (interactive (list (if current-prefix-arg
+                         (read-from-minibuffer "Create scratch buffer: ")
+                       scratch/default-name)))
+  (switch-to-buffer
+   (scratch--get-buffer-create (generate-new-buffer-name buffer-name))))
 
 (defmacro scratch--define-wrapper (fun doc prompt &rest args)
   (declare (debug (commandp stringp stringp &rest form)))
@@ -51,13 +45,10 @@ It is safer if this directory doesn't exist."
                 "\n"
                 "If BUFFER-OR-NAME does not identify an existing buffer, create a\n"
                 "new buffer with that name. Any buffer created this way is\n"
-                "considered a scratch buffer: it is associated to a non-existent\n"
-                "file, is put in `scratch-mode' to avoid unwanted save, and\n"
-                "inherits the current value of `default-directory'.")
+                "considered a scratch buffer: it is put in `scratch-mode' to\n"
+                "avoid data loss, and inherits the current value of \n"
+                "`default-directory'.")
        (interactive ,(concat "B" prompt))
-       (when (and (not scratch/create-when-blank-name)
-                  (string= buffer-or-name ""))
-         (setq buffer-or-name scratch/default-name))
        (let ((buffer (scratch--get-buffer-create buffer-or-name)))
          (,fun buffer ,@args)))))
 
@@ -81,99 +72,69 @@ It is safer if this directory doesn't exist."
 (define-minor-mode scratch-mode
   "Minor mode for temporary buffers.
 
-When in this minor mode, `save-buffer' always prompts for a new
-file name before saving the buffer."
-  :lighter " scratch")
+Emacs prompts to save modified buffers in this mode before
+killing them."
+  :lighter " scratch"
+  ;; Offer saving scratch buffers when exiting emacs
+  (setq buffer-offer-save scratch-mode))
 
-(defun scratch--enable-maybe ()
-  "Enable `scratch-mode' if needed.
+;; Changing major mode doesn't kill `scratch-mode'
+(put 'scratch-mode 'permanent-local t)
 
-Scratch buffers are those associated to files within
-`scratch-mode-directory'."
-  (when (string= scratch/directory
-                 (file-name-directory
-                  (or (buffer-file-name) "")))
-    (scratch-mode 1)))
+;; Saving a scratch buffer should deactivate `scratch-mode'
+(defun scratch--deactivate-after-save ()
+  "Deactivate `scratch-mode' after a buffer has been saved."
+  (when (and scratch-mode
+             (buffer-file-name))
+    (scratch-mode -1)))
 
-(add-hook 'after-change-major-mode-hook #'scratch--enable-maybe)
+(add-hook 'after-save-hook 'scratch--deactivate-after-save)
 
+;; ** Save scratch buffers before killing them
+
+(defun scratch--save-before-kill ()
+  "Offer to save modified `scratch-mode' buffers before killing them."
+  (when (and scratch-mode
+             (not (buffer-file-name))
+             (buffer-modified-p)
+             (yes-or-no-p
+              (format
+               "Buffer `%s' has not been saved. Save it now? "
+               (buffer-name))))
+    (call-interactively #'save-buffer)))
+
+(add-hook 'kill-buffer-hook 'scratch--save-before-kill)
 
 ;; ** Create scratch buffers
-
-(defun scratch--setup ()
-  "Declare the current buffer as a scratch buffer.
-
-Activate `scratch-mode' in the current buffer and give it a file
-name in `scratch-directory'."
-  (scratch-mode 1)
-  (setq buffer-file-name
-        (concat
-         (file-name-as-directory scratch/directory)
-         (buffer-name))))
 
 (defun scratch--get-buffer-create (&optional buffer-or-name)
   "Return the buffer specified by BUFFER-OR-NAME, creating a new one if needed.
 
-If BUFFER-OR-NAME is nil or blank, it defaults to \"*scratch*\".
+If BUFFER-OR-NAME is nil or blank and
+`scratch/create-when-blank-name' is non-nil, it defaults to
+`scratch/default-name'.
 
 If BUFFER-OR-NAME doesn't specify an existing buffer, create a
 new buffer with that name. Any buffer created this way is
-considered a scratch buffer: it is associated to a non-existent
-file, is put in `scratch-mode' to avoid unwanted save, and
-inherits the current value of `default-directory'."
+considered a scratch buffer: it is put in `scratch-mode' to avoid
+data loss, and inherits the current value of
+`default-directory'."
   (setq buffer-or-name (or buffer-or-name ""))
   (let ((buffer (get-buffer buffer-or-name))
         (dd default-directory))
     (unless buffer
-      (when (string= buffer-or-name "")
+      (when (and scratch/create-when-blank-name
+                 (string= buffer-or-name ""))
         (setq buffer-or-name scratch/default-name))
       (setq buffer (generate-new-buffer buffer-or-name))
       (with-current-buffer buffer
-        (scratch--setup)
+        (scratch-mode 1)
         (setq default-directory dd)
-        (set-auto-mode)))
+        (let ((buffer-file-name (concat dd buffer-or-name)))
+          (set-auto-mode))))
     buffer))
 
-
-;; ** Always treat scratch buffers as non-file when saving
-
-(defun scratch--save-buffer--ad-around (orig-fun &rest args)
-  "Prompt for a new file name when saving buffers in `scratch-mode'."
-  (if scratch-mode
-      (let ((buffer-file-name nil))
-        (apply orig-fun args))
-    (apply orig-fun args)))
-(advice-add 'save-buffer :around #'scratch--save-buffer--ad-around)
-
-
-;; ** Temporarily consider scratch buffers as non-file
-
-(defun scratch--ignore--ad-around (orig-fun &rest args)
-  "Temporarily consider scratch buffers as non-file buffers."
-  (let ((scratch--ignore t))
-    (apply orig-fun args)))
-
-(defun scratch--bfn--ad-around (orig-fun &rest args)
-  "Temporarily consider scratch buffers as non-file buffers.
-
-This only happens when `-ignore' is set."
-  (let ((buffer (or (car args) (current-buffer))))
-    (if (and (bound-and-true-p scratch--ignore)
-             (with-current-buffer buffer scratch-mode))
-        nil
-      (apply orig-fun args))))
-(advice-add 'buffer-file-name :around #'scratch--bfn--ad-around)
-
-(defun scratch/ignore (fun)
-  "Arrange for FUN to consider scratch buffers as non-file buffers."
-  (advice-add fun :around #'scratch--ignore--ad-around))
-
-(with-current-buffer "*scratch*"
-  (scratch--setup))
-
-(scratch/ignore 'compile)
-(scratch/ignore 'recompile)
-(scratch/ignore 'magit-status)
+;; * Footer
 
 (provide 'scratch)
 
